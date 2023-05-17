6e789f07ee740d9304906cfa2f3bdd116da3caa7
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-17 13:30:00 +0200

    refactor: update merge_sort method
    Refactored the merge_sort method for readability and efficiency.

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,24 +1,14 @@
 def merge_sort(arr)
-  if arr.length < 2
-    return arr
-  else
-    left_half = merge_sort(arr.slice(0, arr.length/2))
-    right_half = merge_sort(arr.slice(arr.length/2, arr.length))
+  return arr if arr.length < 2
+
+  left_half = merge_sort(arr.slice(0, arr.length/2))
+  right_half = merge_sort(arr.slice(arr.length/2, arr.length))
+
+  sorted = []
+
+  until left_half.empty? || right_half.empty?
+    left_half.first < right_half.first ? sorted << left_half.shift : sorted << right_half.shift
   end
 
-  temp_arr = []
-  left_half_index = 0
-  right_half_index = 0
-
-  while left_half_index < left_half.length && right_half_index < right_half.length
-    if left_half[left_half_index] < right_half[right_half_index]
-      temp_arr << left_half[left_half_index]
-      left_half_index += 1
-    else
-      temp_arr << right_half[right_half_index]
-      right_half_index += 1
-    end
-  end
-
-  temp_arr.concat(left_half.slice(left_half_index..-1), right_half.slice(right_half_index..-1))
+  sorted.concat(left_half, right_half)
 end
